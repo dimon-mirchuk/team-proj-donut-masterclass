40b1fd9e53b93de6435c56858c4c51b2689f029b
Tagir Chanyshev <chanyshev.tagir@gmail.com>
2021-05-21 16:25:38 +0300

    правки

--- src/js/slidedown.js
+++ src/js/slidedown.js
@@ -1,5 +1,5 @@
 $('.actions .btn').on('click', function (e) {
   e.preventDefault();
   $(this).text($(this).text() == 'Read more' ? 'Less' : 'Read more');
-  $(this).closest('article.post').find('.hidden-content').slideToggle();
+  $(this).closest('div.post').find('.hidden-content').slideToggle();
 });
