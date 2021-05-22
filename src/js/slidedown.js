b3b82752c00a133549cbcf4f476b4eecd72b2aca
Tagir Chanyshev <chanyshev.tagir@gmail.com>
2021-05-22 13:02:51 +0300

    1

--- src/js/slidedown.js
+++ src/js/slidedown.js
@@ -1,4 +1,4 @@
-$('.actions .btn').on('click', function (e) {
+$('.action').on('click', function (e) {
   e.preventDefault();
   $(this).text($(this).text() == 'Read more' ? 'Less' : 'Read more');
   $(this).closest('div.post').find('.hidden-content').slideToggle();
